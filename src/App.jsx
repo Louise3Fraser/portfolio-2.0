24fa2d1e03b9f00a3ba695b6f4b93ffd4dc985a6
Louisebfraser <louise.b.fraser@vanderbilt.edu>
2025-10-03 09:20:03 -0400

    init: standalone port

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>
+        Louise Fraser{" "}
+        <span className="grey">
+          is an engineer <span className="ovo">&</span> designer
+        </span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
